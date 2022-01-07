4fcf72b41b8e0e5ff5b3a116b7b8505e06dc1562
Julia Gatina <julia.sarandi@gmail.com>
2022-01-07 10:29:39 +0000

    Implement SQL queries for search properties featues of the app

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -24,11 +24,11 @@ const getUserWithEmail = (email) => {
   const queryString = `SELECT * FROM users WHERE email = $1`;
   const value = [email];
   return pool
-  .query(queryString, value)
-  .then((result) => result.rows[0])
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(queryString, value)
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 exports.getUserWithEmail = getUserWithEmail;
@@ -56,8 +56,8 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  (user) => {
-  const queryString = 
+const addUser = (user) => {
+  const queryString =
     `INSERT INTO users (name, email, password) 
     VALUES ($1, $2, $3)
     RETURNING id`;
@@ -80,22 +80,34 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-    const queryString = 
+  const queryString =
     `SELECT * FROM reservations AS r 
     JOIN properties AS p ON r.property_id = p.id
     JOIN property_reviews AS pr ON p.id = pr.property_id
     WHERE r.guest_id = $1 LIMIT $2`;
-    const values = [guest_id, limit];
-    return pool  
-      .query(queryString, values)
-      .then((result) => result.rows)
-      .catch((err) => {
-        console.log(err.message);
-      });}
+  const values = [guest_id, limit];
+  return pool
+    .query(queryString, values)
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
 
 exports.getAllReservations = getAllReservations;
 
 /// Properties
+
+//helper function for getAllProperties
+const queryConstructor = function(array) {
+  let valueToUse = '';
+  if (array.length > 1) {
+    valueToUse = 'AND';
+  } else if (array.length === 1) {
+    valueToUse = 'WHERE';
+  }
+  return valueToUse;
+};
 
 /**
  * Get all properties.
@@ -103,9 +115,54 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
+const getAllProperties = (options, limit = 10) => {
+  const valuesArray = [];
+
+  // initial query before user input
+  let queryString = `
+  SELECT p.*, AVG(pr.rating) AS average_rating 
+  FROM properties AS p
+  JOIN property_reviews AS pr ON p.id = pr.property_id `;
+
+  // if user adds search criteria then it's added to the value arr and sql query
+  if (options.city) {
+    valuesArray.push(`%${options.city}%`);
+    queryString += queryConstructor(valuesArray) + ` p.city LIKE $${valuesArray.length} `;
+  }
+
+  if (options.owner_id) {
+    valuesArray.push(options.owner_id);
+    queryString += queryConstructor(valuesArray) + ` p.owner_id = $${valuesArray.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    valuesArray.push(options.minimum_price_per_night * 100);
+    queryString += queryConstructor(valuesArray) + ` p.cost_per_night >= $${valuesArray.length} `;
+  }
+  
+  if (options.maximum_price_per_night) {
+    valuesArray.push(options.maximum_price_per_night * 100);
+    queryString += queryConstructor(valuesArray) + ` p.cost_per_night <= $${valuesArray.length} `;
+  }
+
+  
+  queryString += `
+  GROUP BY p.id, pr.rating`
+  
+  if (options.minimum_rating) {
+    valuesArray.push(options.minimum_rating);
+    queryString += ` HAVING pr.rating >= $${valuesArray.length} `;
+  }
+  valuesArray.push(limit);
+  queryString += `
+  ORDER BY p.cost_per_night
+  LIMIT $${valuesArray.length};
+  `;
+
+  console.log(queryString, valuesArray); 
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, valuesArray)
     .then((result) => result.rows)
     .catch((err) => {
       console.log(err.message);
